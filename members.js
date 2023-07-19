944bd5c425f7a7e8e8e2fd9dbea90f6202e15191
Aliyeri <139925933+Aliyeri@users.noreply.github.com>
2023-07-19 02:41:47 +0000

    Copilot second commit

--- members.js
+++ members.js
@@ -1,1 +1,12 @@
-function skillsMember()
+function skillsMember(){
+    return{
+        restrict: 'E',
+        renokateUrl: 'modules/skills/views/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member:'='
+        }
+    };
+}
